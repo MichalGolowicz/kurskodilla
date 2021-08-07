2fb8cb4def39194a98a1097d3ca2ec348b1d7f94
Michał Gołowicz <87637798+MichalGolowicz@users.noreply.github.com>
2021-08-07 15:23:10 +0000

    3phone

--- Main.java
+++ Main.java
@@ -2,13 +2,19 @@ class Main {
   public static void main(String[] args) {
       MobilePhone phone = new MobilePhone("200g");
        System.out.println(phone.weight);
+
+       MobilePhone oldPhone = new MobilePhone("4000g");
+       System.out.println(oldPhone.weight);
+
+       MobilePhone mediumPhone = new MobilePhone("600g");
+       System.out.println(mediumPhone.weight);
   }
 }
 
 class MobilePhone {
   String weight;
 
-  public MobilePhone(Strign weight) {
+  public MobilePhone(String weight) {
     this.weight = weight;
   }
 }
