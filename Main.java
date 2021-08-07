cb80e32d8f4ba78ac2cc172f23fb6642b53d30da
Michał Gołowicz <87637798+MichalGolowicz@users.noreply.github.com>
2021-08-07 16:10:20 +0000

    2 nowe metody

--- Main.java
+++ Main.java
@@ -1,20 +1,30 @@
 class Main {
   public static void main(String[] args) {
-      MobilePhone phone = new MobilePhone("200g");
-       System.out.println(phone.weight);
-
-       MobilePhone oldPhone = new MobilePhone("4000g");
+      MobilePhone phone = new MobilePhone("200g", 700);
+       System.out.println(phone.weight + " " + phone.price);
+       phone.displayValues();
+       MobilePhone oldPhone = new MobilePhone("4000g", 100);
        System.out.println(oldPhone.weight);
 
-       MobilePhone mediumPhone = new MobilePhone("600g");
+       MobilePhone mediumPhone = new MobilePhone("600g", 340);
        System.out.println(mediumPhone.weight);
   }
 }
 
 class MobilePhone {
   String weight;
+  int price;
 
-  public MobilePhone(String weight) {
+  public MobilePhone(String weight, int price) {
     this.weight = weight;
+    this.price = price;
+  }
+
+  public void dispalyValues() {
+    System.out.println(this.weight + " " + this.price);
+  }
+
+  public String getParameters() {
+    return this.weight + " " + this.price;
   }
 }
