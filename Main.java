6cc2a170b2a8289cbbe5d1ebf605f19008f4eb94
MichalGolowicz <87637798+MichalGolowicz@users.noreply.github.com>
2021-08-07 13:44:59 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+      MobilePhone phone = new MobilePhone("200g");
+       System.out.println(phone.weight);
+  }
+}
+
+class MobilePhone {
+  String weight;
+
+  
+}
