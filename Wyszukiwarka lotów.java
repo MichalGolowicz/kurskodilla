fd961f14f73eac7203e382892b5a03032d2d419c
Michał Gołowicz <87637798+MichalGolowicz@users.noreply.github.com>
2021-08-08 09:49:02 +0000

    w budowie

--- Wyszukiwarka lotów.java
+++ Wyszukiwarka lotów.java
@@ -1,7 +1,11 @@
 class Main {
   public static void main(String[] args) {
-
+//tworzę obiekt klasy Flight
+Flight.warszawaDubaj = new Flight("Warszawa", Dubaj);
   }
+//metoda wyświetlająca informacje o locie z metody flightinfo
+Flight.flightinfo();
+system.out.printin(Flightinfo);
 }
 class Flight {
 String departure;
@@ -11,4 +15,7 @@ public Flight(String departure, String arrival) {
 this.departure = departure;
 this.arrival = arrival;
 }
+//metoda zwracająca informację o locie
+public String flightinfo();
+
 }
