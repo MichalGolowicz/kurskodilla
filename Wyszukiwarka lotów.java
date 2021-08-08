d54d4acc72ba73911cc5277f355bd240988c2592
Michał Gołowicz <87637798+MichalGolowicz@users.noreply.github.com>
2021-08-08 10:11:33 +0000

    duplicate class main

--- Wyszukiwarka lotów.java
+++ Wyszukiwarka lotów.java
@@ -2,22 +2,25 @@ class Main {
   public static void main(String[] args) {
 //tworzę obiekty klasy Flight
 Flight warszawaDubaj = new Flight("Warszawa", "Dubaj");
+String parameters = warszawaDubaj.getFlight();
+       System.out.println(parameters);
+
 Flight berlinMoskwa = new Flight("Berlin", "Moskwa");
+
 Flight londynKraków = new Flight("Londyn", "Kraków");
+
   }
-//metoda wyświetlająca informacje o locie z metody flightinfo
-Flight.flightinfo();
-system.out.printin(Flightinfo);
 }
 class Flight {
 String departure;
 String arrival;
-//konstruktor
+//konstruktor klasy Flight
 public Flight(String departure, String arrival) {
 this.departure = departure;
 this.arrival = arrival;
 }
 //metoda zwracająca informację o locie
-public String flightinfo();
-
+public String getFlight() {
+    return this.departure + " " + this.arrival;
+}
 }
