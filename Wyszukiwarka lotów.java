2263452f5e9d1f99be183767382c14fadacd5ef5
Michał Gołowicz <87637798+MichalGolowicz@users.noreply.github.com>
2021-08-08 09:36:05 +0000

    konstruktor

--- Wyszukiwarka lotów.java
+++ Wyszukiwarka lotów.java
@@ -1,6 +1,14 @@
 class Main {
   public static void main(String[] args) {
-  }
-class Flight {
 
+  }
+}
+class Flight {
+String departure;
+String arrival;
+//konstruktor
+public Flight(String departure, String arrival) {
+this.departure = departure;
+this.arrival = arrival;
+}
 }
